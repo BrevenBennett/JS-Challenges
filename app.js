cba429186c94797c4e2e5a2db70ee33add0f3deb
BrevenBennett <Breven.stars@gmail.com>
2022-12-14 11:31:32 -0600

    finished beginner challenges

---
+++ app.js
@@ -0,0 +1,116 @@
+// ~~~~~~~~~~BEGINNER CHALLENGES~~~~~~~~~~
+// Q1 Return the sum of two numbers
+// function addition(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(addition(3, 2))
+
+// Q2 Convert hours into seconds
+// function hoursIntoSeconds(hours) {
+//     return hours * 3600
+// }
+
+// console.log(hoursIntoSeconds(10))
+
+// Q3 Calculate the perimeter of a rectangle
+// function calcPerimeter(width, length) {
+//     return 2 * (width + length)
+// }
+
+// console.log(calcPerimeter(20, 10))
+
+// Q4 Calculate the area of a triangle
+// function calcArea(base, height) {
+//     return 0.5 * base * height
+// }
+
+// console.log(calcArea(10, 10))
+
+// Q5 Extend a string
+// function appendFrontend(str) {
+//     return str + 'Frontend'
+// }
+
+// console.log(appendFrontend('Apple'))
+// console.log(appendFrontend('Banana'))
+
+// Q6 Greater than 100?
+// function sumGreaterThan100(num1, num2) {
+//   return num1 + num2 > 100;
+// }
+
+// console.log(sumGreaterThan100(50, 60));
+// console.log(sumGreaterThan100(100, -50));
+
+// Q7 Less than or equal to zero
+// function lessThanOrEqualToZero(num) {
+//     return num <= 0
+// }
+
+// console.log(lessThanOrEqualToZero(3))
+// console.log(lessThanOrEqualToZero(0))
+// console.log(lessThanOrEqualToZero(-2))
+
+// Q8 Opposite boolean
+// function oppositeBoolean(bool) {
+//     return !bool
+// }
+
+// console.log(oppositeBoolean(true))
+// console.log(oppositeBoolean(false))
+
+// Q9 Is not the number 0
+// function isNotZero(elem) {
+//     return elem !== 0
+// }
+
+// console.log(isNotZero(5))
+// console.log(isNotZero(0))
+// console.log(isNotZero(-10))
+
+// Q10 Calculate the remainder of two numbers
+// function calcRemainder(num1, num2) {
+//     return num1 % num2
+// }
+
+// console.log(calcRemainder(4, 2))
+// console.log(calcRemainder(7, 8))
+// console.log(calcRemainder(9, 8))
+
+// Q11 Is the number odd?
+// function isOdd(num) {
+//   return num % 2 !== 0;
+// }
+
+// console.log(isOdd(1));
+// console.log(isOdd(2));
+// console.log(isOdd(3));
+
+// Q12 If a number is even, return 1, otherwise return -1
+// function booleanInteger(num) {
+//   return num % 2 === 0 ? 1 : -1;
+// }
+
+// console.log(booleanInteger(1));
+// console.log(booleanInteger(2));
+// console.log(booleanInteger(5));
+
+// Q13 Check if a user is logged in AND subscribed
+// function isLoggedInAndSubscribed(str1, str2) {
+//     return str1 === 'LOGGED_IN' && str2 === 'SUBSCRIBED'
+// }
+
+// console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+// console.log(isLoggedInAndSubscribed('LOGGED_IN', 'UNSUBSCRIBED'))
+// console.log(isLoggedInAndSubscribed('LOGGED_OUT', 'SUBSCRIBED'))
+
+// Q14 Check if a user is logged in OR subscribed
+// function isLoggedInOrSubscribed(str1, str2) {
+//     return str1 === 'LOGGED_IN' || str2 === 'SUBSCRIBED'
+// }
+
+// console.log(isLoggedInOrSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+// console.log(isLoggedInOrSubscribed('LOGGED_IN', 'UNSUBSCRIBED'))
+// console.log(isLoggedInOrSubscribed('LOGGED_OUT', 'SUBSCRIBED'))
+// console.log(isLoggedInOrSubscribed('LOGGED_OUT', 'UNSUBSCRIBED'))
