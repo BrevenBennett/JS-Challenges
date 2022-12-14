205b7c9504ec18126b44ef505f1060454012d1ff
BrevenBennett <Breven.stars@gmail.com>
2022-12-14 15:43:18 -0600

    practiced several medium questions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ~~~~~~~~~~BEGINNER CHALLENGES~~~~~~~~~~
+//              ~~~~~~~~~~BEGINNER CHALLENGES~~~~~~~~~~
 // Q1 Return the sum of two numbers
 // function addition(num1, num2) {
 //     return num1 + num2
@@ -114,3 +114,159 @@
 // console.log(isLoggedInOrSubscribed('LOGGED_IN', 'UNSUBSCRIBED'))
 // console.log(isLoggedInOrSubscribed('LOGGED_OUT', 'SUBSCRIBED'))
 // console.log(isLoggedInOrSubscribed('LOGGED_OUT', 'UNSUBSCRIBED'))
+
+//              ~~~~~~~~~~MEDIUM CHALLENGES~~~~~~~~~~
+// Q1 Falsy or Truthy?
+// function falsyOrTruthy(val1, val2) {
+//     return (!val1) ? val1 : val2
+// }
+
+// console.log(falsyOrTruthy(0, 500))
+// console.log(falsyOrTruthy(false, 100))
+// console.log(falsyOrTruthy(true, 'Dog'))
+
+// Q2 Return the length of any given array
+// function arrLength(arr) {
+//     return arr.length
+// }
+
+// console.log(arrLength([1, 2, 3]))
+// console.log(arrLength([5, 0, -4, 1]))
+// console.log(arrLength([]))
+
+// Q3 Get the last element in an array
+// function lastElem(arr) {
+//     return arr[arr.length-1]
+// }
+
+// console.log(lastElem([3, 2, 0, 6, 2]))
+// console.log(lastElem(['dog', 'cat', 'ball']))
+// console.log(lastElem([null, 5, false]))
+
+// Q4 Find the sum of an array
+// function arrSum(arr) {
+//   let sum = 0;
+//   for (i = 0; i < arr.length; ++i) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// console.log(arrSum([2, 2, 2]));
+// console.log(arrSum([100, 200, 500]));
+// console.log(arrSum([0, -5, -10]));
+
+// Q5 Add up the numbers from a single number
+// function progressiveSum(num) {
+//   let sum = 0;
+//   for (i = 1; i <= num; ++i) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(progressiveSum(3));
+// console.log(progressiveSum(4));
+// console.log(progressiveSum(600));
+
+// Q6 Calculate the time
+// function calcTime(seconds) {
+//   let timerMinutes = Math.floor(seconds / 60);
+//   let timerSeconds = seconds % 60;
+
+//   if (timerMinutes.toString().length === 1) {
+//     timerMinutes = '0' + timerMinutes
+//   }
+
+//   if (seconds >= 60 && seconds <= 69) {
+//     timerSeconds = '0' + timerSeconds
+//   }
+
+//   return timerMinutes + ':' + timerSeconds
+// }
+
+// console.log(calcTime(66));
+// console.log(calcTime(50));
+// console.log(calcTime(700));
+
+// Q7 Find the largest number
+// function getMax(arr) {
+//   let max = arr[0];
+//   for (i = 0; i < arr.length; ++i) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(getMax([5, 100, 0]));
+// console.log(getMax([12, 10, -20]));
+// console.log(getMax([-300, -100, -200]));
+
+// Q8 Reverse a String
+// function reverseString(str) {
+    // 3 Methods
+
+    // 1. Incrementing Loop
+    // let reversedString = ''
+    // for (i = 0; i < str.length; ++i) {
+    //     reversedString = str[i] + reversedString
+    // }
+    // return reversedString
+
+    // 2. Decrementing Loop
+    // let reversedString = ''
+    // for (i = str.length - 1; i >= 0; --i) {
+    //     reversedString += str[i]
+    // }
+    // return reversedString
+
+    // 3. Array Reverse Property
+    // return str.split('').reverse().join('')
+// }
+
+// console.log(reverseString('abc'))
+// console.log(reverseString('David'))
+// console.log(reverseString('This is cool'))
+
+// Q9 Turn every element in an array to 0
+// function convertToZeros(arr) {
+    // 3 Methods
+    // 1. For Loop
+    // let newArr = []
+    // for (i = 0; i < arr.length; ++i) {
+    //     newArr[i] = 0
+    // }
+    // return newArr
+
+    // 2. Array Fill
+    // return new Array(arr.length).fill(0)
+    
+    // 3. Array Map
+    // return arr.map(elem => 0)
+// }
+
+// console.log(convertToZeros([5, 100, 10]))
+// console.log(convertToZeros([12]))
+// console.log(convertToZeros([1, 2, 3, 4, 5]))
+
+// Q10 Filter out all the 'Apples'
+// function noApples(arr) {
+    // 2 Methods
+    // 1. For Loop
+    // let noApples = []
+    // for (i = 0; i < arr.length; ++i) {
+    //     if (arr[i] !== 'Apple') {
+    //         noApples.push(arr[i])
+    //     }
+    // }
+    // return noApples
+
+    // 2. Array Filter
+//     return arr.filter(elem => elem !== 'Apple')
+// }
+
+// console.log(noApples(['Banana', 'Apple', 'Orange', 'Apple']))
+// console.log(noApples(['Tomato', 'Orange', 'Banana']))
+// console.log(noApples(['Banana', 'Orange', 'Apple']))
